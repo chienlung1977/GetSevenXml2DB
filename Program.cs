b4d77c3789552e350c2cfc9d98eb5c1f52d69792
chienlung1977 <chienlung1977@gmail.com>
2015-08-20 22:02:23 +0800

    v1.01.001

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LCL.Base;
+using LCL.Business;
+
+namespace GetSevenXml2DB
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Seven s = new Seven();
+            s.Start();
+
+        }
+
+
+
+        
+
+    }
+}
